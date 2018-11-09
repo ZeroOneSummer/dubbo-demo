b0973bbec207b58b8c31e0bb7aac525bb360a80a
jiangping <jiangping@dimaidt.com>
2018-11-09 10:36:44 +0800

    Dubbo RPC demo finished

--- consumer/src/main/java/com/dubbo/controller/HelloController.java
+++ consumer/src/main/java/com/dubbo/controller/HelloController.java
@@ -1,24 +1,30 @@
 package com.dubbo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.dubbo.service.HelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author Administrator
+ * @author jp
  * @Description: TODO
  * @date 2018/11/8 18:29
  */
 @RestController
 public class HelloController {
+    static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    @Autowired
-    private HelloConsumer helloConsumer;
+    @Reference(version = "1.0.0")
+    private HelloService helloService;
 
     @RequestMapping("/show/{name}")
-    public void show(@PathVariable("name") String name){
-        helloConsumer.show(name);
+    public String show(@PathVariable("name") String name){
+        logger.info("consumer Dubbo RPC to provider is start... ");
+
+        return helloService.show(name);
     }
 
 }
--- provider/src/main/java/com/dubbo/service/impl/HelloServiceImpl.java
+++ provider/src/main/java/com/dubbo/service/impl/HelloServiceImpl.java
@@ -2,18 +2,27 @@ package com.dubbo.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.dubbo.service.HelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 /**
- * @author Administrator
+ * @author jp
  * @Description: TODO
  * @date 2018/11/8 18:13
  */
-@Service
+@Component
+@Service(version = "1.0.0", timeout = 30000)
 public class HelloServiceImpl implements HelloService {
+    static Logger logger = LoggerFactory.getLogger(HelloServiceImpl.class);
 
     @Override
-    public void show(String name) {
-        System.out.println("------------>welcome to provider:" + name);
+    public String show(String name) {
+        String msg = "show: welcome use dubbo: " + name;
+        logger.info(msg);
+        logger.info("consumer Dubbo RPC to provider is finised ");
+
+        return msg;
     }
 
 }
